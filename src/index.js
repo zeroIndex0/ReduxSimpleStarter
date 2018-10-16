b97ea6f366d0349ec1efc984eaa0b85ae2fb6702
zeroIndex0 <sagtogi@gmail.com>
2018-10-15 20:38:37 -0400

    Changed some things and testing git

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = "";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//a quick a dirty useage of the YTSearch function
+// YTSearch({key: API_KEY, term: 'ylyl'}, data => console.log(data));
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new componet. This componet should produce some HTML
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'ylyl'}, videos => { 
+            this.setState({videos: videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+// Take this componet's generated HTML and put it on the page (in the DOM)
+ReactDOM.render( <App />, document.querySelector('.container'));
